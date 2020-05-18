561e0d4d744d94afc6678836a5815cb3c8fb76d8
Gavin Gu <gavingu0130@hotmail.com>
2020-05-18 13:55:20 +0800

    init

---
+++ src/main/java/com/gavin/includekindsofcompnent/IncludeKindsOfCompnentApplication.java
@@ -0,0 +1,13 @@
+package com.gavin.includekindsofcompnent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncludeKindsOfCompnentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncludeKindsOfCompnentApplication.class, args);
+	}
+
+}
