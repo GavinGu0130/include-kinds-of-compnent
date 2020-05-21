e192bf7d603b4ecf53622b88af51f0d544112e38
Gavin Gu <gavingu0130@hotmail.com>
2020-05-21 21:15:23 +0800

    no message

--- src/main/java/com/gavin/includekindsofcompnent/design/iterator_pattern/advance/AdvanceIteratorEntry.java
+++ src/main/java/com/gavin/includekindsofcompnent/design/iterator_pattern/advance/AdvanceIteratorEntry.java
@@ -1,7 +1,14 @@
 package com.gavin.includekindsofcompnent.design.iterator_pattern.advance;
 
 public class AdvanceIteratorEntry {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+            func();
+            Thread.sleep(3000);
+        }
+    }
+
+    public static void func(){
         ICustomCollection list = new CustomArrayList();
         for (int i=0; i<16; i++){
             list.add("g" + i);
